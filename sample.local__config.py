69f5658ae982012b62fbeff3eb17fe51d93cd1b7
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2023-08-06 12:04:20 -0300

    Start

---
+++ sample.local__config.py
@@ -0,0 +1,15 @@
+#Nombre que identifica al server que ejecuta
+SERVER_IDENTIFIER = '' 
+
+LOC_MYSQL_H = '127.0.0.1'
+LOC_MYSQL_U = ''
+LOC_MYSQL_P = ''
+LOC_MYSQL_DB = ''
+
+LOC_BNC_AK = ''
+LOC_BNC_SK = ''
+LOC_BNC_TESNET = True #True/False
+
+LOC_TLGRM_TK = ""
+
+LOC_TLGRM_CHATID = 0
