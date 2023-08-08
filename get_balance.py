d3e73ce22428fc72fe4c61c808fbb4fa296b4270
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2023-08-08 08:18:57 -0300

    Update get_balance.py

--- get_balance.py
+++ get_balance.py
@@ -6,11 +6,16 @@ import functions as fn
 client = Client(local.LOC_BNC_AK, local.LOC_BNC_SK,
                     testnet=local.LOC_BNC_TESNET)
 
+asset = 'USDT'
+balance = client.get_asset_balance(asset)
+print(asset,' -> ',balance)
 info = client.get_exchange_info()
 
 for dato in info:
     if dato=='symbols':
         for i in info[dato]:
+
             asset = i['baseAsset']
-            balance = client.get_asset_balance(asset)
-            print(asset,balance)
+            if i['quoteAsset']=='USDT':
+                balance = client.get_asset_balance(asset)
+                print(asset,' -> ',balance)
