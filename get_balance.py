f865585830a941c9925d3efad83bb51c5ba533bd
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2023-08-08 08:14:00 -0300

    Create get_balance.py

---
+++ get_balance.py
@@ -0,0 +1,16 @@
+from binance.client import Client
+import local__config as local
+import my_logging as mylog
+import functions as fn
+
+client = Client(local.LOC_BNC_AK, local.LOC_BNC_SK,
+                    testnet=local.LOC_BNC_TESNET)
+
+info = client.get_exchange_info()
+
+for dato in info:
+    if dato=='symbols':
+        for i in info[dato]:
+            asset = i['baseAsset']
+            balance = client.get_asset_balance(asset)
+            print(asset,balance)
